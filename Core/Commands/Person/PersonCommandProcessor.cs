6b57dae68577cfa565f1083cdf3e9bb48a3c3e89
Mohsin Jamadar <mohsin.softtech@gmail.com>
2019-04-04 18:02:41 +1100

    Add files via upload

---
+++ Core/Commands/Person/PersonCommandProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Family.Core.Factories;
+using Family.Core.Interfaces;
+using Family.Core.Models;
+using Family.Repository;
+
+namespace Family.Core.Commands
+{
+    public class PersonCommandProcessor : CommandProcessor<Person>
+    {
+        public PersonCommandProcessor(IRepository<Person> personRepository, ICommandDictionary<Person> personcommandDictionary) : base(personRepository, personcommandDictionary) { }
+
+    }
+
+   
+}
