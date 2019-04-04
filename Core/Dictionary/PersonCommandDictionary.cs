98576d29ce6b03d4d479cab3d9c1e354ba4e833e
Mohsin Jamadar <mohsin.softtech@gmail.com>
2019-04-04 18:04:43 +1100

    Add files via upload

---
+++ Core/Dictionary/PersonCommandDictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Family.Core.Commands;
+using Family.Core.Interfaces;
+using Family.Core.Models;
+
+namespace Family.Core.Factories
+{
+    public class PersonCommandDictionary :ICommandDictionary<Person>
+    {
+
+        public  Dictionary<string, ICommand<Person>> GetDictionary()
+        {
+            return new Dictionary<string, ICommand<Person>>
+            {
+                { "ADD_CHILD", new AddChildCommand() },
+                { "GET_RELATIONSHIP", new GetRelativesCommand() }
+            };
+        }
+    }
+
+   
+}
